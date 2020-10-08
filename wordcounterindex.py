96a22a2a9a95ca7630b4946e775067518bfdaeed
Okan Erkaptan <okanerkaptan@okanbook.speedport.ip>
2020-10-08 22:26:13 +0200

    woerter Zaehlen geht zwar aber ist nicht optimal

--- wordcounterindex.py
+++ wordcounterindex.py
@@ -1,5 +1,15 @@
-print ("Hello World")
+import sys
 
-print("kaka")
 
-print ("ok")
+from collections import Counter
+def word_count(filename):
+    with open(filename) as f:
+        return Counter(f.read().split())
+
+zeichenMitLeerzeichen = 0
+
+counter = word_count('/Users/okanerkaptan/Downloads/WordcountTestText.rtf')
+for i in counter:
+    print(i, ':', counter[i])
+    zeichenMitLeerzeichen = zeichenMitLeerzeichen +1 
+print("Das ist die gesamte anzahl an Zeichen :", zeichenMitLeerzeichen) #stimmt doch nicht ist nicht das gesamte anzahl an Zeichen lel
