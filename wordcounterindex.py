eb53c1aed7a9c30435afbe7afee5ddd1c76ac1f3
Okan Erkaptan <okanerkaptan@okanbook.speedport.ip>
2020-10-08 22:36:16 +0200

    ok

--- wordcounterindex.py
+++ wordcounterindex.py
@@ -13,3 +13,11 @@ for i in counter:
     print(i, ':', counter[i])
     zeichenMitLeerzeichen = zeichenMitLeerzeichen +1 
 print("Das ist die gesamte anzahl an Zeichen :", zeichenMitLeerzeichen) #stimmt doch nicht ist nicht das gesamte anzahl an Zeichen lel
+#oder
+
+user_str = input('Enter String')
+l = user_str.split()
+d = {}
+for j in l:
+    d[j] = d.get(j, 0)+1
+print(d)
